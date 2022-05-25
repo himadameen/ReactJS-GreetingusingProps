6eacf45ceccdfac6b02b656a93b3473431390bed
Himmad Ameen <93907951+himadameen@users.noreply.github.com>
2022-05-25 17:30:35 +0530

    my changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React from "react";
+
+const Welcome = (abc) => {
+  return (
+    <>
+      <h1>Hey {abc.name}!</h1>
+      <h2>Welcome to Newton School</h2>
+    </>
+  );
+};
+
+export default Welcome;
